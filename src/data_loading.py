d5309cf4d29dc585f20e269986138c52ad91a708
AlessandroCiano <alessandrociano02@gmail.com>
2025-02-13 17:35:24 +0000

    We can build ML models now!

--- src/data_loading.py
+++ src/data_loading.py
@@ -25,7 +25,7 @@ class OxfordPetDataset:
             name='oxford_iiit_pet',
             split=['train[:90%]', 'train[90%:]', 'test'],
             shuffle_files=True,
-            as_supervised=True,
+            as_supervised=False, # with False each example is a dictionary ['image', 'label', 'segmentation_mask']
             with_info=True
         )
         
@@ -45,9 +45,42 @@ class OxfordPetDataset:
         print(f'Number of test samples: {self.num_test_examples}')
 
     def one_hot_encoding(self):
-        self.train = self.train_raw.map(lambda image, label: (image, tf.one_hot(label, self.num_classes)))
-        self.val = self.val_raw.map(lambda image, label: (image, tf.one_hot(label, self.num_classes)))
-        self.test = self.test_raw.map(lambda image, label: (image, tf.one_hot(label, self.num_classes)))
+        # Apply one-hot encoding to all sets
+        self.train_raw = self.train_raw.map(lambda example: one_hot_encoding(example, self.num_classes))
+        self.val_raw = self.val_raw.map(lambda example: one_hot_encoding(example, self.num_classes))
+        self.test_raw = self.test_raw.map(lambda example: one_hot_encoding(example, self.num_classes))
+
+    def res_norm(self, resize_shape=(128, 128)):
+        # Resize and normalize
+        self.train_raw = self.train_raw.map(lambda example: resize_norm(example, resize_shape))
+        self.val_raw = self.val_raw.map(lambda example: resize_norm(example, resize_shape))
+        self.test_raw = self.test_raw.map(lambda example: resize_norm(example, resize_shape))
+
+# one-hot encoding of a dataset
+"""Converts the label of data dict to one-hot encoding while preserving the dictionary structure."""
+
+def one_hot_encoding(example, num_classes):
+    return {
+        "image": example["image"],
+        "label": tf.one_hot(example["label"], num_classes),
+        "segmentation_mask": example["segmentation_mask"]
+    }
+
+# single image resize and normalization
+def resize_norm_single(image, resize_shape):
+    """Resize and normalize a single image."""
+    image = tf.image.resize(image, resize_shape)
+    image = tf.cast(image, tf.float32) / 255.0
+    return image
+
+# resize and normalize a dataset
+def resize_norm(example, resize_shape):
+    # Resize and normalize
+    return {
+        "image": resize_norm_single(example["image"], resize_shape),
+        "label": example['label'],
+        "segmentation_mask": resize_norm_single(example["segmentation_mask"], resize_shape)
+    }
 
 
 def show_examples(train_raw, ds_info):
@@ -60,19 +93,4 @@ def get_value_counts(ds):
     label_counts = pd.Series(label_list).value_counts(sort=True)
     print(label_counts)
 
-def view_image(dataset, ds_info, get_label_name):
-    # Build the custom function to display image and label name
-    image, label = next(iter(dataset))
-    print('Image shape: ', image.shape)
-    plt.imshow(image)
-    _ = plt.title(get_label_name(label))
 
-
-# resize and normalize images
-def resize_normalize(data, input_shape=(128, 128)):
-    # Resize and normalize
-    image = tf.image.resize(data['image'], input_shape)
-    image = tf.cast(image, tf.float32) / 255.0
-    mask = tf.image.resize(data['segmentation_mask'], input_shape)
-    mask = tf.cast(mask, tf.float32) / 255.0
-    return image, mask
